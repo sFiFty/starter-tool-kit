171e0d86d847a99f40e9b54cdc117d016c3794f3
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2019-03-21 08:18:41 +0200

    added stylings to about page

--- app/screens/About/About.tsx
+++ app/screens/About/About.tsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
 
+import './styles.scss';
+
 export const About = () => {
   return (
-    <div>
-      <p>Hello, my name is Alex. I am an owner of the "Starter Toolkit".</p>
-      <p>The main goal of "Started Toolkit" is to help quickly build new applications.</p>
-      <p>I am as a software engineer know how much time should be spent to build the skeleton of the application. It is quite a long period of time. </p>
-      <p>I will build a web tool which helps you to not spend the time to develop structure.</p>
+    <div className="about-container">
+      <div className="is-size-4">
+        <p>Hello, my name is Alex. I am an owner of the "Starter Toolkit".</p>
+        <p>The main goal of "Started Toolkit" is to help quickly build new applications.</p>
+        <p>I am as a software engineer know how much time should be spent to build the skeleton of the application. It is quite a long period of time. </p>
+        <p>I will build a web tool which helps you to not spend the time to develop structure.</p>
+      </div>
     </div>
   );
 }
