00085ca78447f13ef76019fa96a0f2815dfb356e
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2019-03-29 08:35:51 +0200

    started working on modal functionality

--- app/components/AuthModal/AuthModal.tsx
+++ app/components/AuthModal/AuthModal.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 import { Input } from 'components/Input';
+import { Modal } from 'components/Modal';
 import './styles.scss';
 
 export const AuthModal = () => {
   return (
-    <div className="auth-modal-container">
-      <div className="modal is-active">
-        <div className="modal-background"></div>
-        <div className="modal-content">
-          <h1>To continue Log in or Sign up into the system </h1>
-          <input className="input" type="email" placeholder="Text input" />
-          <input className="input" type="password" placeholder="Text input" />
-        </div>
-        <button className="modal-close is-large" aria-label="close"></button>
-      </div>
-    </div>
+    <Modal>
+      <h1>To continue Log in or Sign up into the Starter Toolkit </h1>
+      <Input type="email" placeholder="Text input" />
+      <Input  type="password" placeholder="Text input" />
+    </Modal>
   );
 }
---
+++ app/components/Modal/Modal.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+export interface IModalProps {
+  children: any;
+}
+
+export const Modal = (props: IModalProps) => {
+  return (
+    <div className="auth-modal-container">
+      <div className="modal is-active">
+        <div className="modal-background"></div>
+        <div className="modal-content">
+          {props.children}
+        </div>
+        <button className="modal-close is-large" aria-label="close"></button>
+      </div>
+    </div>
+  );
+}
