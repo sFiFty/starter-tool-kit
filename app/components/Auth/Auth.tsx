1b98d0a7f3b2db3b67dc7fcd41006db3c440ffd2
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2019-03-29 09:36:27 +0200

    working on auth

--- app/components/Auth/Auth.tsx
+++ app/components/Auth/Auth.tsx
@@ -6,10 +6,21 @@ import './styles.scss';
 export const Auth = () => {
   return (
     <div className="auth-modal-container">
-      <Modal>
-        <h1>To continue Log in or Sign up into the Starter Toolkit </h1>
-        <Input type="email" placeholder="Text input" />
-        <Input  type="password" placeholder="Text input" />
+      <Modal fullScreen>
+        <div className="auth-modal-content">
+          <div className="landing">
+            <div className="landing-content">
+              Some landing content
+            </div>
+          </div>
+          <div className="auth">
+            <div className="auth-content">
+              <h1>To continue Log in or Sign up into the Starter Toolkit </h1>
+              <Input type="email" placeholder="Text input" />
+              <Input  type="password" placeholder="Text input" />
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
--- app/components/Modal/Modal.tsx
+++ app/components/Modal/Modal.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react';
 
+import { addClassNames } from 'utils/component-helpers';
+import './styles.scss';
+
 export interface IModalProps {
   children: any;
-  className?: string
+  fullScreen: boolean;
+  contentClassName?: string
 }
 
-export const Modal = (props: IModalProps) => {
+const DEFAULT_CONTENT_CLASSES = 'modal-content';
+const FULL_SCREEN_CLASS = 'full-screen';
+
+export const Modal = ({ contentClassName, children, fullScreen }: IModalProps) => {
+  let contentClassNames = addClassNames(contentClassName, DEFAULT_CONTENT_CLASSES);
+  if (fullScreen) contentClassNames = addClassNames(FULL_SCREEN_CLASS, contentClassNames);
   return (
-    <div className="modal is-active">
+    <div className="modal modal-container is-active">
       <div className="modal-background"></div>
-      <div className="modal-content">
-        {props.children}
+      <div className={contentClassNames}>
+        {children}
       </div>
       <button className="modal-close is-large" aria-label="close"></button>
     </div>
