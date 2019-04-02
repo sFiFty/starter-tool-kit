d73be4e7ca3758cb6d93f6654dcca1ea713e832c
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2019-04-02 10:01:06 +0300

    added new account styles

--- app/components/Auth/Auth.tsx
+++ app/components/Auth/Auth.tsx
@@ -28,10 +28,24 @@ export const Auth = () => {
           <div className="auth">
             <div className="inner-content">
               <h1 className="is-size-2 is-size-3-mobile has-text-black has-text-centered">Hello again!</h1>
-              <Input type="email" placeholder="Email" size={IInputSizes.medium} />
-              <Input  type="password" placeholder="Password" size={IInputSizes.medium} />
-              <div className="has-text-centered">
-                <Button type={IButtonTypes.primary} size={IButtonSizes.medium}>Login</Button>
+              <form action="login">
+                <Input type="email" placeholder="Email" size={IInputSizes.medium} />
+                <Input  type="password" placeholder="Password" size={IInputSizes.medium} />
+                <div className="has-text-centered">
+                  <Button className="is-size-5" type="submit" styleType={IButtonTypes.primary} size={IButtonSizes.medium}>
+                    SIGN IN
+                  </Button>
+                </div>
+              </form>
+              <div className="new-account">
+                <p className="has-text-centered is-size-5">
+                  Don’t have an account?
+                </p>
+                <div className="has-text-centered">
+                  <Button className="is-size-5" type="button" styleType={IButtonTypes.success} size={IButtonSizes.medium}>
+                    SIGN UP
+                  </Button>
+                </div>              
               </div>
             </div>
           </div>
--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -3,25 +3,26 @@ import * as React from 'react';
 import { addClassNames } from 'utils/component-helpers';
 import { IButtonTypes, getButtonType, getButtonSize, IButtonSizes } from './config';
 
-export interface IButtonProps {
+export interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
-  type?: IButtonTypes;
   size?: IButtonSizes;
   children: any;
+  styleType?: IButtonTypes;
 }
 
 const DEFAULT_CLASS = 'button';
 
-export const Button = ({ className = '', type, size, children }: IButtonProps) => {
+export const Button = ({ className = '', size, children, styleType, ...rest }: IButtonProps) => {
   let classNames = addClassNames(className, DEFAULT_CLASS);
 
-  const buttonTypeClass = getButtonType(type);
+  const buttonTypeClass = getButtonType(styleType);
   classNames = addClassNames(classNames, buttonTypeClass);
 
   const buttonSizeClass = getButtonSize(size);
   classNames = addClassNames(classNames, buttonSizeClass);
+
   return (
-    <button className={classNames}>
+    <button className={classNames} {...rest} >
       {children}
     </button>
   )
