adab627bcc9c74ad75e94536aa450c5ccf96680d
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2019-04-02 08:40:06 +0300

    added button sizes

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
 
 import { addClassNames } from 'utils/component-helpers';
-import { IButtonTypes, getButtonType } from './config';
+import { IButtonTypes, getButtonType, getButtonSize, IButtonSizes } from './config';
 
 export interface IButtonProps {
   className?: string;
   type?: IButtonTypes;
+  size?: IButtonSizes;
   children: any;
 }
 
 const DEFAULT_CLASS = 'button';
 
-export const Button = ({ className = '', type, children }: IButtonProps) => {
+export const Button = ({ className = '', type, size, children }: IButtonProps) => {
   let classNames = addClassNames(className, DEFAULT_CLASS);
+
   const buttonTypeClass = getButtonType(type);
   classNames = addClassNames(classNames, buttonTypeClass);
+
+  const buttonSizeClass = getButtonSize(size);
+  classNames = addClassNames(classNames, buttonSizeClass);
   return (
     <button className={classNames}>
       {children}
--- app/components/Button/config.ts
+++ app/components/Button/config.ts
@@ -17,7 +17,27 @@ export enum buttonType {
   danger = 'is-danger',
 }
 
+export enum IButtonSizes {
+  small = 'small',
+  normal = 'normal',
+  medium = 'medium',
+  large = 'large'
+}
+
+export enum buttonSize {
+  default = '',
+  small = 'is-small',
+  normal = 'is-normal',
+  medium = 'is-medium',
+  large = 'is-large'
+}
+
 export const getButtonType = (type?: IButtonTypes): buttonType => {
   if (!type) return buttonType.default;
   return buttonType[type];
+};
+
+export const getButtonSize = (size?: IButtonSizes): buttonSize => {
+  if (!size) return buttonSize.default;
+  return buttonSize[size];
 };
