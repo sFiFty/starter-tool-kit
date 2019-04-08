5637dd46b3e1359950e15cec670c8e5c55f29b14
Oleksandr Rudin <alexandr.rudin.my@gmail.com>
2019-04-08 09:02:35 +0300

    added arrow-bottom icon

--- app/components/Icon/config.ts
+++ app/components/Icon/config.ts
@@ -1,13 +1,17 @@
 import CloseIcon from 'static/icons/close.svg';
+import ArrowBottomIcon from 'static/icons/arrow-bottom/svg';
 
 const close: string = CloseIcon;
+const arrowBottom: string = ArrowBottomIcon;
 
 export enum IIconsNames {
   close = 'close',
+  arrowBottom = 'arrowBottom',
 }
 
 export const icons = {
-  close: CloseIcon
+  close: CloseIcon,
+  arrowBottom: ArrowBottomIcon,
 }
 
 export const getIconByName = (name: IIconsNames): string => {
